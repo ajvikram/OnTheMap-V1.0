dc189be06d2c724db660e93eff2995cf74d2f91e
Singh, Ajay (as396y) <as396y@att.com>
2018-01-13 19:25:53 -0500

    first commit

---
+++ OnTheMap V1.0/Controller/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  OnTheMap V1.0
+//
+//  Created by Ajay Singh on 1/12/18.
+//  Copyright © 2018 ATT. All rights reserved.
+//
+
+import Foundation
---
+++ OnTheMap V1.0/Model/Udacity-Stu/UdacityClient.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityClient.swift
+//  OnTheMap V1.0
+//
+//  Created by Ajay Singh on 1/11/18.
+//  Copyright © 2018 ATT. All rights reserved.
+//
+
+import Foundation
